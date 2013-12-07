e34c16b3f56c29cc2af56c4b79f8de6b65facb44
kvanderd <me@katharine.org>
2013-12-06 17:31:03 -0800

    cleaning up landing page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,25 +1,14 @@
 class ProfilesController < InheritedResources::Base
-<<<<<<< HEAD
 
   def index
     @profiles = Profile.all
+    @category = Category.all
   end
 
   def show
     @profile = Profile.find(params[:id])
-    
+    @category = Category.all
   end
-=======
-	
-	def index
-		@profiles = Profile.all
-	end
 
-
-	def show
-		@profile = Profile.find(params[:id])
-	end
-
->>>>>>> f3e2d4c248c87dbd88e70673a3dffbdeb89c7e23
 
 end
